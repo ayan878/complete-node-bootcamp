ed1bfca23b0dc8b55dd89eba2e6aa04e3b47d5cd
ayan878 <ayanraza197@gmail.com>
2023-07-04 00:07:38 +0530

    NodeJs

--- 1-node-farm/starter/index.js
+++ 1-node-farm/starter/index.js
@@ -1,4 +1,5 @@
-const fs = require('fs');
-const textIn = fs.readFileSync('./txt/input.txt', 'utf-8');
+const fs = require("fs");
+const textIn = fs.readFileSync("txt/input.txt", "utf-8");
 console.log(textIn);
-
+const textOut =`this is avacado: ${textIn}.\n created on ${Date.now()}`;
+fs.writeFileSync('./txt/Output.txt', textOut);

b44f4f262b6bff847cd2e7ba3dcbdc650a53fb53
ayan878 <ayanraza197@gmail.com>
2023-07-04 13:40:28 +0530

    NodeJs

--- 1-node-farm/starter/index.js
+++ 1-node-farm/starter/index.js
@@ -1,4 +1,5 @@
 const fs = require("fs");
+const http = require("http");
 
 //blocking, Synchronous way
 // const textIn = fs.readFileSync("txt/input.txt", "utf-8");
@@ -8,19 +9,29 @@ const fs = require("fs");
 // console.log(textOut);
 
 //Non-blocking Asynchronous  way
-fs.readFile("./txt/start.txt", "utf-8", (err, data1) => {
-  if (err) return console.log("Error!💥");
-  console.log(data1);
+// fs.readFile("./txt/start.txt", "utf-8", (err, data1) => {
+//   if (err) return console.log("Error!💥");
+//   console.log(data1);
   //NodeJs is start doing in background and will not block the code and it immidiatly move in next code
   //thats why is first print the line no 18 than 13
-  fs.readFile("./txt/read-this.txt", "utf-8", (err, data2) => {
-    console.log(data2);
-    fs.readFile("./txt/append.txt", "utf-8", (err, data3) => {
-      console.log(data3);
-      fs.writeFile("./txt/final.txt", `${data2}\n ${data3}`, "utf-8", (err) => {
-        console.log("File is Written ✔😊");
-      });
-    });
-  });
+//   fs.readFile("./txt/read-this.txt", "utf-8", (err, data2) => {
+//     console.log(data2);
+//     fs.readFile("./txt/append.txt", "utf-8", (err, data3) => {
+//       console.log(data3);
+//       fs.writeFile("./txt/final.txt", `${data2}\n ${data3}`, "utf-8", (err) => {
+//         console.log("File is Written ✔😊");
+//       });
+//     });
+//   });
+// });
+// console.log("will read file!");
+
+///////////////////////////////////////////////////////
+// Server 
+const server = http.createServer((req, res)=>{
+    res.end('Hello from the server!');   
 });
-console.log("will read file!");
+
+server.listen(8000,'127.0.0.1',()=>{
+    console.log('Listening to request on port 8000');
+});

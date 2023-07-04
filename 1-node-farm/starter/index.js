06a3ca0b69273f3030e868a3159d2b0a46be7b6a
ayan878 <ayanraza197@gmail.com>
2023-07-05 02:16:35 +0530

    NodeJs

--- 1-node-farm/starter/index.js
+++ 1-node-farm/starter/index.js
@@ -31,6 +31,20 @@ const url = require("url");
 
 ///////////////////////////////////////////////////////
 // Server
+const replaceTemplate = (temp, product) => {
+  let output = temp.replace(/{%PRODUCTNAME%}/g, product.productName);
+  output = temp.replace(/{%IMAGE%}/g, product.image);
+  output = output.replace(/{%PRICE%}/g, product.price);
+  output = output.replace(/{%FROM%}/g, product.from);
+  output = output.replace(/{%NUTRIENTS%}/g, product.nutrients);
+  output = output.replace(/{%QUANTITY%}/g, product.quantity);
+  output = output.replace(/{%DESCRIPTION%}/g, product.Description);
+  output = output.replace(/{%ID%}/g, product.id);
+
+  if (!product.organic)
+    output = output.replace(/{%NOT_ORGANIC%}/g, "not-organic"); // 'not-organic' is class name which display is none.
+  return output;
+};
 const tempOverview = fs.readFileSync(
   `${__dirname}/templates/template-overview.html`,
   "utf-8"

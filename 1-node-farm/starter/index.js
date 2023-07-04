7a14f6b045124411c2d913a2177ed93646aff129
ayan878 <ayanraza197@gmail.com>
2023-07-04 12:55:45 +0530

    NodeJs

--- 1-node-farm/starter/index.js
+++ 1-node-farm/starter/index.js
@@ -1,5 +1,25 @@
 const fs = require("fs");
-const textIn = fs.readFileSync("txt/input.txt", "utf-8");
-console.log(textIn);
-const textOut =`this is avacado: ${textIn}.\n created on ${Date.now()}`;
-fs.writeFileSync('./txt/Output.txt', textOut);
+
+//blocking, Synchronous way
+// const textIn = fs.readFileSync("txt/input.txt", "utf-8");
+// console.log(textIn);
+// const textOut =`this is avacado: ${textIn}.\n created on ${Date.now()}`;
+// fs.writeFileSync('./txt/Output.txt', textOut);
+// console.log(textOut);
+
+//Non-blocking Asynchronous  way
+fs.readFile("./txt/start.txt", "utf-8", (err, data1) => {
+  console.log(data1);
+  //NodeJs is start doing in background and will not block the code and it immidiatly move in next code
+  //thats why is first print the line no 18 than 13
+  fs.readFile("./txt/read-this.txt", "utf-8", (err, data2) => {
+    console.log(data2);
+    fs.readFile("./txt/append.txt", "utf-8", (err, data3) => {
+      console.log(data3);
+      fs.writeFile('./txt/final.txt',`${data2}\n ${data3}`,'utf-8', err=>{
+        console.log('File is Written ✔😊');
+      })
+    });
+  });
+});
+console.log("will read file!");

ac5fef2dfc51aef8d7aaf9bbe618dced01fb246f
ayan878 <ayanraza197@gmail.com>
2023-07-06 23:25:57 +0530

    NodeJs

--- 1-node-farm/starter/index.js
+++ 1-node-farm/starter/index.js
@@ -62,11 +62,11 @@ const data = fs.readFileSync(`${__dirname}/dev-data/data.json`, "utf-8");
 const dataObj = JSON.parse(data);
 
 const server = http.createServer((req, res) => {
-  const pathName = req.url;
-
+  // console.log(req,res);
+  const { query, pathname } = url.parse(req.url, true);
+  // const pathname =req.url;
   //OVERVIEW PAGE
-  if (pathName === "/" || pathName === "/overview") {
-
+  if (pathname === "/" || pathname === "/overview") {
     res.writeHead(200, { "Content-type": "text/html" });
 
     const cardsHtml = dataObj
@@ -76,16 +76,17 @@ const server = http.createServer((req, res) => {
     res.end(output);
   }
   //PRODUCT PAGE
-  else if (pathName === "/product") {
-     const product = dataObj
-       .map((el) => replaceTemplate(tempProduct, el))
-       .join("");
-     const output = tempProduct.replace(tempProduct, product);
-     res.end(output);
+  else if (pathname === "/product") {
+   res.writeHead(200, {
+     "Content-type": "text/html",
+   });
+   const product = dataObj[query.id];
+   const output = replaceTemplate(tempProduct, product);
+   res.end(output);
     // res.end(tempProduct);
   }
   //API
-  else if (pathName === "/api") {
+  else if (pathname === "/api") {
     res.writeHead(200, { "Content-type": "application/json" });
     res.end(data);
   }

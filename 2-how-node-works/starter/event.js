0a5f544bfcdc69982fe35b89d25a04d2d0d9ff95
ayan878 <ayanraza197@gmail.com>
2024-05-07 00:16:15 +0530

    event

---
+++ 2-how-node-works/starter/event.js
@@ -0,0 +1,12 @@
+const EventEmitter = require("events");
+
+const myEmitter = new EventEmitter();
+
+myEmitter.on("newSale", () => {
+  console.log("There was a new sale");
+});
+myEmitter.on("newSale", () => {
+  console.log("Customer name: Ayan");
+});
+
+myEmitter.emit("newSale");

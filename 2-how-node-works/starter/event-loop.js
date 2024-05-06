977b76626cc98fb87b2dfc8214cbcde2344a182e
ayan878 <ayanraza197@gmail.com>
2024-05-06 18:33:05 +0530

    event loop

--- 2-how-node-works/starter/event-loop.js
+++ 2-how-node-works/starter/event-loop.js
@@ -1,10 +1,13 @@
-They are not in particular order  This code is not have I/O cycle so its not running inside event loop it is not running inside any callback
-const fs = require("fs");
-setTimeout(() => console.log("Timer 1 finished"), 0);
-setImmediate(() => console.log("Immediate 1 finished"));
+// !They are not in particular order  This code is not have I/O cycle so its not running inside event loop it is not running inside any callback
+// const fs = require("fs");
+// setTimeout(() => console.log("Timer 1 finished"), 0);
+// setImmediate(() => console.log("Immediate 1 finished"));
 
-fs.readFile("test-file.txt", () => console.log("I/O finished"));
-console.log("Hello from the top-level code");
+// fs.readFile("test-file.txt", () => console.log("I/O finished"));
+// console.log("Hello from the top-level code");
+
+
+// !this code have I/O cycle so its runs inside event loop 
 
 const fs = require("fs");
 

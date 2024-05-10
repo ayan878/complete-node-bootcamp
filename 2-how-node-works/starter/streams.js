afc0b516bebe4d1f1d0c83e9f946356d4c4c06f1
ayan878 <ayanraza197@gmail.com>
2024-05-10 23:34:54 +0530

    stream

--- 2-how-node-works/starter/streams.js
+++ 2-how-node-works/starter/streams.js
@@ -1,16 +1,21 @@
 const fs = require("fs");
 const server = require("http").createServer();
-console.log(server);
 
-server.on("require", (req, res) => {
+server.on("request", (req, res) => {
   // Solution 1
-  fs.readFile("test-file.text", (err, data) => {
+  fs.readFile("test-file.txt", (err, data) => {
     if (err) {
       console.log(err);
+      res.statusCode = 500;
+      res.end("Error reading file");
+    } else {
+      res.statusCode = 200;
+      res.setHeader("Content-Type", "text/plain");
       res.end(data);
     }
   });
 });
+
 server.listen(8000, "127.0.0.1", () => {
   console.log("Listening...");
 });

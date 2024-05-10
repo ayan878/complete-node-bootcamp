1729cd27e9e4f811a686ea820963e18743094bda
ayan878 <ayanraza197@gmail.com>
2024-05-10 23:59:28 +0530

    stream

--- 2-how-node-works/starter/streams.js
+++ 2-how-node-works/starter/streams.js
@@ -25,6 +25,10 @@ server.on("request", (req, res) => {
   readable.on("end", () => {
     res.end();
   });
+  readable.on("error", (err) => {
+    res.statusCode(500);
+    res.end("File not found");
+  });
 });
 
 server.listen(8000, "127.0.0.1", () => {

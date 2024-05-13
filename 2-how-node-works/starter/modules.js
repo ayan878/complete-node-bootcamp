fd83f71c22640e5dc36b4ccb2cdb364950ffe828
ayan878 <ayanraza197@gmail.com>
2024-05-13 20:45:11 +0530

    module

--- 2-how-node-works/starter/modules.js
+++ 2-how-node-works/starter/modules.js
@@ -5,10 +5,24 @@
 
 // const { constants } = require("buffer");
 
-// modules.exports
+// modules.exports : to export a single class/variable/function from one module to another module.
 const C = require("./test-module-1");
 const calc1 = new C();
 console.log(calc1.add(2, 5));
 
+//exports : to export multiple variables/functions from one module to another.
+const cal2 = require("./test-module-2");
+console.log(cal2.add(2, 5));
 
-//exports
+// we can also use the power of ES6 object destructuring
+const { add, multiply, divide } = require("./test-module-2");
+console.log(add(2, 5));
+console.log(multiply(2, 5));
+console.log(divide(2, 5));
+
+// caching
+require("./test-module-3")();
+
+// both are retrieve from cache instead of loading  module again
+require("./test-module-3")();
+require("./test-module-3")();

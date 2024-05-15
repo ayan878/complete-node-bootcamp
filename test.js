336241ff4b113de8ad6859a9ef95d5a9ac25e5d6
ayan878 <ayanraza197@gmail.com>
2024-05-15 21:36:08 +0530

    Interviews Prep

---
+++ test.js
@@ -0,0 +1,91 @@
+// debugger
+// function square(num) {
+//   return num * num;
+// }
+
+// function displaySquare(fun) {
+//   console.log("Square is " + fun(5));
+// }
+
+// displaySquare(square);
+
+// let user = {
+//   username: "Ayan Raza",
+//   arrFun: () => {
+//     console.log("I'm " + this.username);
+//   },
+//   regFuntion() {
+//     console.log("i'm " + this.username);
+//   },
+// };
+// user.arrFun();
+// user.regFuntion();
+
+// console.log(this);
+// "use strict";
+// function x() {
+//   console.log(this);
+// }
+// x();
+// window.x();
+
+// const obj = {
+//   a: 10,
+//   x: function () {
+//     console.log(this);
+//   },
+// };
+// obj.x();
+
+// function sayHello(greetingFunction) {
+//   greetingFunction();
+// }
+// console.log("Hey there!");
+
+// sayHello(function () {}); // Output: Hey there!
+
+////
+// function square(num) {
+//   return num * num;
+// }
+// function displaySquare(fun) {
+//   console.log("Square is " + fun(5));
+// }
+// displaySquare(function square(num) {
+//   return num * num;
+// });
+
+let user = {
+  firstname: "Ayan",
+  lastname: "Raza",
+};
+
+let printFullName = function (hometown, state) {
+  console.log(
+    this.firstname + " " + this.lastname + " from " + hometown,
+    state
+  );
+};
+
+// first parameter is always refer to this variable and the later argument refer to the function
+printFullName.call(user, "Siwan", "Bihar");
+let user2 = {
+  firstname: "Belal",
+  lastname: "Ahmed",
+};
+
+let user3 = {
+  firstname: "Neha",
+  lastname: "Singh",
+};
+
+// function borrowing
+printFullName.call(user2, "Ziradei", "Bihar");
+
+// apply() method
+printFullName.apply(user3, ["Pachrukhi", "Bihar"]);
+
+// bind() method
+let printMyName = printFullName.bind(user, "Siwan", "Bihar");
+console.log(printMyName); // it print the function
+printMyName();
